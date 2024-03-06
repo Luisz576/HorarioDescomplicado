12710c7aaf27ca8ccbab06088380d9fdb647dfdb
Luisz576 <56451779+Luisz576@users.noreply.github.com>
2024-03-05 21:07:00 -0300

    start

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from 'cors'
+import http from 'http'
+import path from 'path'
+
+const __dirname = path.resolve()
+const port = 5000
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(cors())
+
+app.use('/', express.static(path.join(__dirname, '/src/public')))
+
+server.listen(port, () => {
+    console.log(`Running at port: ${port}`)
+})
