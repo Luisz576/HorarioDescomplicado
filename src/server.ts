3d46d82c8fd95a196c13b5fcbb52e2d76dfae595
= <lrferlinalves@gmail.com>
2024-07-30 21:47:14 -0300

    little changes[

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 import http from 'http'
 import globalRoutes from './app/route/global_routes'
 import apiRoutes from './app/route/api_routes'
-import publicApiRoutes from './app/route/public_api_routes'
+import authApiRoutes from './app/route/auth_api_routes'
 import prisma from './app/service/prisma'
 
 const port = 5000
@@ -15,11 +15,11 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/', globalRoutes)
-app.use('/api/auth/', publicApiRoutes)
+app.use('/api/auth/', authApiRoutes)
 app.use('/api/v1/', apiRoutes)
 
 server.listen(port, () => {
-    console.log(`Running at port: ${port}`)
+  console.log(`Running at port: ${port}`)
 })
 
 server.on('close', () => {
